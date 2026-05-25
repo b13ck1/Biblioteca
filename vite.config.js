e881f21999e60b2c95f38af1a314e378a21e623c
Jose <jose.hinostroza@usil.pe>
2026-05-24 21:46:46 -0600

    Fix vite config2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': env.VITE_API_URL
-      }
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
